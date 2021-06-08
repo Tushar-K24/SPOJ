a6ff59cf71a94e98967c4962bb131b56de939a0c
AyushK-26 <85547808+AyushK-26@users.noreply.github.com>
2021-06-08 14:23:58 +0530

    updated code
    removed unnecessary parenthesis

--- first100/prime1.cpp
+++ first100/prime1.cpp
@@ -3,14 +3,9 @@
 using namespace std;
 
 bool isPrime(int n){
-	if(n==1){
-		return false;
-	}
-	for(int i=2;i*i<=n;i++){
-		if(n%i==0){
-			return false;
-		}
-	}
+	if(n==1) return false;
+	for(int i=2;i*i<=n;i++)
+		if(n%i==0) return false;
 	return true;
 }
 
@@ -18,13 +13,9 @@ int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		int n,m;
-		cin>>n>>m;
-		for(int i=n;i<=m;i++){
-			if(isPrime(i)){
-				cout<<i<<endl;
-			}
-		}		
+		int n,m; cin>>n>>m;
+		for(int i=n;i<=m;i++)
+			if(isPrime(i)) cout<<i<<endl;
 	}
 	return 0;
 }
